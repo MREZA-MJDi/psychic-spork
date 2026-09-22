3d6d3f64cbfe4e52b69a6850a34e90479d56b3af
MREZA-MJDi <rm2025studio@gmail.com>
2026-09-22 13:57:06 +0330

    chore add configurable Janan store name

--- config/app.php
+++ config/app.php
@@ -15,6 +15,8 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
+    'store_name' => env('JANAN_STORE_NAME', 'Janan'),
+
     /*
     |--------------------------------------------------------------------------
     | Application Environment
