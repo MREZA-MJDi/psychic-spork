aafdefa2943b2baec6e5b679575f06f05acbe75e
MREZA-MJDi <rm2025studio@gmail.com>
2026-09-22 13:50:00 +0330

    chore: remove demo user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserSeeder::class,
             BrandSeeder::class,
             CategorySeeder::class,
             ProductSeeder::class,
